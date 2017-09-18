e5bb7fba2fa3751bedd9b5fa3ad892712a1fc92f
Alvin Grant <alving.nyc@gmail.com>
2017-09-18 11:34:04 -0400

    Style homepage work item ui
    Change homepage works title
    
    Update number of work items called from api

--- app/js/components/index/index.component.js
+++ app/js/components/index/index.component.js
@@ -5,7 +5,7 @@ angular.
 		controller: ['$routeParams', '$http',
 			function IndexController($routeParams, $http) {
 				var self = this;
-				$http.get('http://alvingrant.com/api/wp-json/wp/v2/posts?per_page=1&categories=2').then(function(response) {
+				$http.get('http://alvingrant.com/api/wp-json/wp/v2/posts?per_page=4&categories=2').then(function(response) {
 					self.items = response.data;
 					console.log(self.items);
 				});
