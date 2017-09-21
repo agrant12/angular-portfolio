a6f7f1f3696425e7494cc7c8d4fb6d4c0ebd6a80
Alvin Grant <alving.nyc@gmail.com>
2017-09-21 08:31:44 -0400

    Develop filter function to remove tags rendered in journal component

--- app/js/components/index/index.component.js
+++ app/js/components/index/index.component.js
@@ -5,10 +5,15 @@ angular.
 		controller: ['$routeParams', '$http',
 			function IndexController($routeParams, $http) {
 				var self = this;
-				$http.get('http://alvingrant.com/api/wp-json/wp/v2/posts?per_page=4&categories=2').then(function(response) {
+				$http.get('http://alvingrant.com/api/wp-json/wp/v2/posts?per_page=1&categories=2').then(function(response) {
 					self.items = response.data;
 					console.log(self.items);
 				});
 			}
 		]
+	}).
+	filter('removeTags', function() {
+		return function(text) {
+			return  text ? String(text).replace(/<[^>]+>/gm, '') : '';
+		};
 	});
